a1b0de0ad423c2e63f397a4652d7efaa2594020f
Eugene Wu <eugenewu@eugenes-mbp.lan>
2020-08-19 18:05:57 -0400

    version bump

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ except ImportError:
 from setuptools import setup, find_packages
 
 setup(name="wuutils",
-      version="0.0.20",
+      version="0.0.21",
       description="Dumb utility functions I use",
       license="MIT",
       author="Eugene Wu",
